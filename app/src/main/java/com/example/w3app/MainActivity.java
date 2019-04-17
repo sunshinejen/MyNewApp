39f7a32e7219387025ffdb28de5aedc0460c2872
JVillacis <jennifer.villacis@seattlecentral.edu>
2019-04-16 20:13:08 -0700

    adds log to main activity so make code easier to identify

--- app/src/main/java/com/example/w3app/MainActivity.java
+++ app/src/main/java/com/example/w3app/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.w3app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    //contant tag
+    private static final String TAG= "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG,"WE are in the on create method of Main activity");
     }
 }
